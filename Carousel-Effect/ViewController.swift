dbf674691e985dc143a0bfabcade9fa15dc561d3
dongeunshin <dongeunshin@umass.edu>
2021-12-08 15:32:27 +0900

    Initial Commit

---
+++ Carousel-Effect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carousel-Effect
+//
+//  Created by dong eun shin on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Carousel-EffectUITests/Carousel_EffectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Carousel_EffectUITestsLaunchTests.swift
+//  Carousel-EffectUITests
+//
+//  Created by dong eun shin on 2021/12/08.
+//
+
+import XCTest
+
+class Carousel_EffectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
